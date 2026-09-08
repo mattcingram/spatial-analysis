8d7dd43d302176298cb022c75400fcf6919dab46
Matt Ingram <40709229+mattcingram@users.noreply.github.com>
2026-09-08 16:18:27 -0400

    Update ingram_rep_brassetal2020_4_matrixW_20260726.R

--- example1_ghana/code/ingram_rep_brassetal2020_4_matrixW_20260726.R
+++ example1_ghana/code/ingram_rep_brassetal2020_4_matrixW_20260726.R
@@ -3,7 +3,7 @@
 # Ingram, Matt 
 # Reproduction of Brass et al. (2020) in Political Geography
 # created: 2023-04-23
-# last updated: 2026-07-26
+# last updated: 2026-09-08
 # steps here: generate connectivity matrix (W) and graph
 #
 ################################################################################
